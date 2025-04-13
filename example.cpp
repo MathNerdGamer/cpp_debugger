338e58ce5229d029eb240bbd32940bc96d7aae6b
MathNerdGamer <mathnerd@anothermathblog.com>
2025-04-13 17:15:23 -0500

    Finally putting my C++ debugger on GitHub.

--- example.cpp
+++ example.cpp
@@ -3,20 +3,19 @@
 
 int main(int argc, char **argv)
 {
-    int num = 1;
-    std::string str = "string";
-    std::string another_great_variable_name = "What am I doing with my life?";
+	int num = 1;
+	std::string str = "string";
+	std::string another_great_variable_name = "What am I doing with my life?";
 
-    DEBUG_OUTPUT(num, str, another_great_variable_name);
+	DEBUG_OUTPUT(num, str, another_great_variable_name);
     // Yes, it is that easy.
 
-    return 0;
+	return 0;
 }
 
 /*
 Output:
 
-num = 1, str = string, another_great_variable_name = What am I doing with my
-life?
+num = 1, str = string, another_great_variable_name = What am I doing with my life?
 
 */
